87c3eee5f05f1233c585dd6e1b6bd379f7ccc015
jmr <jmr@makina-corpus.com>
2021-07-21 18:45:04 +0200

    Add file_name in export form, view and template tags

--- templatetags/pdf_export.py
+++ templatetags/pdf_export.py
@@ -1,4 +1,6 @@
 from django.template.defaulttags import register
+from django.utils.datetime_safe import datetime
+from django.utils.text import slugify
 from django.utils.translation import gettext as _
 
 from osis_export.api.forms import ExportForm
@@ -12,6 +14,7 @@ def pdf_export_task(
     async_task_name,
     async_task_description,
     async_task_ttl=None,
+    file_name=None,
 ):
     if hasattr(object_list, "object_list"):
         object_list = object_list.object_list.first()
@@ -20,6 +23,10 @@ def pdf_export_task(
     model_name = object_list.__class__.__name__
     # Then to instantiate the model from it's app just do :
     # ClassName = apps.get_model(app_label, model_name)
+
+    if file_name is None:
+        today = datetime.today().date().isoformat()
+        file_name = slugify(f"export-{async_task_name}-{today}")
 
     return {
         "export_button_text": _("Export in PDF file"),
@@ -34,6 +41,7 @@ def pdf_export_task(
                 # 'next' is used to redirect to the same exact result page after export
                 "next": context.request.get_full_path(),
                 "type": ExportTypes.PDF.name,
+                "file_name": file_name,
             }
         ),
     }

b7dbe2e5370054be1829da96bc21986ca6edd18e
jmr <jmr@makina-corpus.com>
2021-07-26 17:11:07 +0200

    Test manager not_generated function on ExportManager

---
+++ tests/test_managers.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+from base.tests.factories.person import PersonFactory
+from osis_async.models import AsyncTask
+from osis_async.models.enums import TaskStates
+from osis_async.tests.factory import AsyncTaskFactory
+from osis_export.models import Export
+
+
+class TestManager(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.async_task = AsyncTaskFactory()
+        Export.objects.create(
+            job_uuid=cls.async_task.uuid, person=cls.async_task.person
+        )
+        async_task = AsyncTaskFactory(state=TaskStates.PROCESSING.name)
+        Export.objects.create(
+            job_uuid=async_task.uuid,
+            person=async_task.person,
+        )
+
+    def test_export_manager_not_generated_function_only_returns_pending_tasks(self):
+        self.assertEqual(Export.objects.count(), 2)
+        self.assertEqual(Export.objects.not_generated().count(), 1)
+        self.async_task.state = TaskStates.DONE.name
+        self.async_task.save()
+        self.assertEqual(Export.objects.not_generated().count(), 0)

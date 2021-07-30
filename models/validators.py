086415dff94a18ea09e697282ce6719530f33a3e
jmr <jmr@makina-corpus.com>
2021-07-30 14:59:28 +0200

    Validation on Export file field to check for correct inheritance

--- models/validators.py
+++ models/validators.py
@@ -1,3 +1,8 @@
-def validate_class_inherit_from_export_mixin(classname):
-    # TODO When export mixin will be done, check if the class inherit from one of them
-    pass
+from django.utils.module_loading import import_string
+
+from osis_export.contrib.export_mixins import ExportMixin, FileExportMixin
+
+
+def validate_export_mixin_inheritance(classname):
+    BaseClass = import_string(classname)
+    return issubclass(BaseClass, (ExportMixin, FileExportMixin))

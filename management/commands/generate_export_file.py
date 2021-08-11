1a24889daa28d971c43ba9cf8de102707cd4276d
jmr <jmr@makina-corpus.com>
2021-08-11 17:37:57 +0200

    Update related async task in file generation command

--- management/commands/generate_export_file.py
+++ management/commands/generate_export_file.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+from django.utils import timezone
+from django.utils.html import format_html
 from django.utils.module_loading import import_string
 from django.utils.translation import gettext as _
 
@@ -14,22 +16,36 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for export in Export.objects.not_generated():
+            job_uuid = export.job_uuid
+            # Update the related async task first
+            update_task(
+                job_uuid,
+                progression=1,
+                state=TaskStates.PROCESSING.name,
+                started_at=timezone.now(),
+            )
             # Import and instantiate the base view class
             base_class_instance = import_string(export.called_from_class)()
             # generate the wanted file by calling the method on mixin
             file = base_class_instance.generate_file(filters=export.filters)
             # save file into an Upload object in order to reuse osis_document
-            token = save_raw_upload(
-                file,
-                "{}{}".format(
-                    export.file_name, base_class_instance.get_file_extension()
-                ),
-                base_class_instance.get_mimetype(),
-            )
+            file_extension = base_class_instance.get_file_extension()
+            file_mimetype = base_class_instance.get_mimetype()
+            file_name = "{}{}".format(export.file_name, file_extension)
+            token = save_raw_upload(file, file_name, file_mimetype)
             export.file = [token.token]
             export.save()
-            update_task(export.job_uuid, progression=100, state=TaskStates.DONE.name)
-            payload = "{}: {}".format(
-                _("Your document is available here"), token.upload.file.url
+            # and finally update the related async task again
+            update_task(
+                job_uuid,
+                progression=100,
+                state=TaskStates.DONE.name,
+                completed_at=timezone.now(),
+            )
+            payload = format_html(
+                "{}: <a href='{}' target='_blank'>{}</a>",
+                _("Your document is available here"),
+                token.upload.file.url,
+                file_name,
             )
             WebNotification.objects.create(person=export.person, payload=payload)

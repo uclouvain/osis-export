1249c0071731dec957cd909fed75da92a33fb6a9
Sebastien Corbin <seb.corbin@gmail.com>
2021-11-02 12:01:38 +0100

    Fix usage of gettext in models

--- osis_export/models/export.py
+++ osis_export/models/export.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils.module_loading import import_string
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from base.models.person import Person
 from osis_document.contrib import FileField
